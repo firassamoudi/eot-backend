649dae33c4eb9cc8a37f2c3acb32740a8236ef0a
firassamoudi <36210857+firassamoudi@users.noreply.github.com>
2022-09-19 15:19:36 +0100

    localhost

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://api.eot.tn',
+  url: 'http://localhost',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '0b79dee0327cecaae2406dda076c29f9'),
